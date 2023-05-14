b6e5780318b651b163ff9511d4e904e3f16543a7
Muhammad Ammar <89547501+Ammar2k@users.noreply.github.com>
2023-05-14 14:39:48 +0500

    Add files via upload

---
+++ habit-tracker/app.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from pymongo import MongoClient
+
+from routes import pages
+
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    app.db = client.habit
+
+    app.register_blueprint(pages)
+    return app
